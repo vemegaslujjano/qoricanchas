8cd5e5a98f0f6d94bcd47d2b543dc4247631865c
vemegaslujjano <venegaslujjano@gmail.com>
2025-03-20 21:33:33 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import handlebars from 'vite-plugin-handlebars';
 
 export default {
   base: '/qoricanchas/',
+  build: {outDir: 'build'},
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'partials'),
