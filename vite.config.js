abb97907118d38281b33132fcd7e358ff6784896
vemegasslujjano <venegaslujjano@gmail.com>
2025-03-22 16:40:15 -0500

    Agregar carpeta build para producción

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import handlebars from 'vite-plugin-handlebars';
 
 export default {
   base: '/qoricanchas/',
-  build: {outDir: 'build'},
+  build: {
+    outDir: 'build', // La carpeta de salida es "build"
+  },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'partials'),
